4cd13d618e33e9111bf036cf04869da83eef9f45
XCh1an <142370435+XCh1an@users.noreply.github.com>
2023-08-22 10:19:44 +0800

    variables.py
    :)

---
+++ variables.py
@@ -0,0 +1,5 @@
+temp = 27#int(input("Tempertaure(°C) : "))
+hum = 30#int(input("Humidity(%) : "))
+cor = 45#int(input("Chance of rain(%) : "))
+
+print("Today's temperture is "+str(temp)+"°C with a humidity "+str(hum)+"%. Chance of rain : "+str(cor)+"%.")
